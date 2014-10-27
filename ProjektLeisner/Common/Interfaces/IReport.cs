fb626606adc3d42dd32812d17ec2bac5fee44006
Troels Brødsgaard <tr@arbr.dk>
2014-10-27 12:16:38 +0100

    add Model and Common

---
+++ ProjektLeisner/Common/Interfaces/IReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    interface IReport
+    {
+        DateTime Date { get; set; }
+        List<IPeeSpot> PeeSpots { get; set; }
+    }
+}
