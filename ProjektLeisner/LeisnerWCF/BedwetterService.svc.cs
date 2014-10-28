ea7365f4ac5c86f2e446a68129f7e8bc130c5a95
Troels Brødsgaard <tr@arbr.dk>
2014-10-28 10:38:06 +0100

    add not-implemented service

---
+++ ProjektLeisner/LeisnerWCF/BedwetterService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+using LeisnerWCF.Model;
+
+namespace LeisnerWCF
+{
+    public class BedwetterService : IBedwetterService
+    {
+        public Customer GetCustomer(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SubmitQuestionnaire(Questionnaire questionnaire, Patient patient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
