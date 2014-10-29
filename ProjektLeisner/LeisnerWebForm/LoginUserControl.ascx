fd02bcb9664162bbd4045a123db8d044b6cbe1b8
troxdk <desmaagraa@gmail.com>
2014-10-29 13:40:21 +0100

    login done

---
+++ ProjektLeisner/LeisnerWebForm/LoginUserControl.ascx
@@ -0,0 +1,10 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="LoginUserControl.ascx.cs" Inherits="LeisnerWebForm.LoginUserControl" %>
+
+<asp:Login ID="LoginControl" runat="server" UserNameLabelText="Email:" UserNameRequiredErrorMessage="Email is required.">
+</asp:Login>
+
+
+<asp:Label ID="ErrorMessage" runat="server" ForeColor="Red" Text=""></asp:Label>
+
+
+
---
+++ ProjektLeisner/LeisnerWebForm/LoginUserControl.ascx.cs
@@ -0,0 +1,31 @@
+﻿using LeisnerWebForm.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LeisnerWebForm
+{
+    public partial class LoginUserControl : System.Web.UI.UserControl
+    {
+        BedwetterServiceClient client = new BedwetterServiceClient();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Customer customer = client.GetCustomer(LoginControl.UserName);
+            
+            if (customer.Email != null)
+            {
+                
+            }
+            else
+            {
+                ErrorMessage.Text = LoginControl.UserNameRequiredErrorMessage;
+            }
+
+            
+        }
+    }
+}
