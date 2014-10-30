c6ae443e0f55d254f6aca88f7fde8c4a91ce1402
troxdk <desmaagraa@gmail.com>
2014-10-30 12:39:22 +0100

    WPF lækker

--- ProjektLeisner/LeisnerWPF/MainWindow.xaml.cs
+++ ProjektLeisner/LeisnerWPF/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.
 
 namespace LeisnerWPF
 {
@@ -28,13 +29,60 @@ namespace LeisnerWPF
             InitializeComponent();
 
             client = new BedwetterServiceClient();
+
+            gridQ.Visibility = Visibility.Hidden;
+
+            
         }
 
         private void btnGetCustomers_Click(object sender, RoutedEventArgs e)
         {
-            
+            listCustomer.ItemsSource = client.GetAllCustomers();
             
 
         }
+
+        private void listCustomer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Customer customer = (Customer)listCustomer.SelectedItem;
+
+            listPatient.ItemsSource = customer.Patients;
+        }
+
+        private void listPatient_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Patient patient = (Patient)listPatient.SelectedItem;
+
+            listQuestionnaire.ItemsSource = patient.Questionnaires;
+        }
+
+        private void listQuestionnaire_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Questionnaire questionnaire = (Questionnaire)listQuestionnaire.SelectedItem;
+
+            gridQ.Visibility = Visibility.Visible;
+
+            listWetBeds.ItemsSource = questionnaire.WetBeds;
+            listToiletVisits.ItemsSource = questionnaire.ToiletVisits;
+
+            txtQComment.Text = questionnaire.Comment;
+            txtQContact.Text = questionnaire.PleaseContact.ToString();
+            txtQDate.Text = questionnaire.Date.ToShortDateString();
+            txtQMotivation.Text = questionnaire.Motivation.ToString();
+        }
+
+        private void listWetBeds_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            WetBed wetBed = (WetBed)listWetBeds.SelectedItem;
+
+           
+        }
+
+        private void listToiletVisits_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
     }
 }
